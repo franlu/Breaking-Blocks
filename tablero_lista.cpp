19299c1584d38b7061a42806822165ab86044baf
Fran Lucena <fran.lucena@gmail.com>
2011-06-19 13:56:53 +0200

    constructors: first lines

--- tablero_lista.cpp
+++ tablero_lista.cpp
@@ -3,13 +3,18 @@
 tablero::tablero(const pair<unsigned int,unsigned int>& par){
 
 	_tam = par;
+	
+	for (int i = 0 ; i < _tam.first * _tam.second; i++ )
+
 
 }
 
 tablero::tablero(const pair<unsigned int,unsigned int>& par, const list<char>& bloques){
 
 	_tam = par;
-	_matriz = bloques;
+	list<int>::iterator it;
+	for ( it=bloques.begin() ; it != bloques.end(); it++)
+																																	
 
 }
 
@@ -20,7 +25,7 @@ tablero::preguntar_posicion(const pair<unsigned int,unsigned int>& par) const {
 
 }
 
-tablero::eliminar_posicion(const pair<unsigned int,unsigned int>& par){
+int tablero::eliminar_posicion(const pair<unsigned int,unsigned int>& par){
 
 
 
@@ -29,19 +34,19 @@ tablero::eliminar_posicion(const pair<unsigned int,unsigned int>& par){
 
 pair<unsigned int,unsigned int> tablero::size() const {
 
-	return this._tam;
+	return _tam;
 
 }
 
-//implementar
+
 ostream& operator<< (ostream& os, const tablero& t){
 
-	for (int i=0; i<t.fil(); i++){
+/*	for (int i=0; i<t.fil(); i++){
 		os << endl;
 		for (int j=0; j<t.col(); j++)
 			os << t(i,j) << ' ';
 	}
-
+*/
 return os;
 
 }
