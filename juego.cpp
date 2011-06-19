209fd07573b9480d083ffd817adb432f78b5cf78
Fran Lucena <fran.lucena@gmail.com>
2011-06-19 13:54:12 +0200

    first lines

--- juego.cpp
+++ juego.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+
+#include "breaking_blocks.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	
+	cout << "BREAKING BLOCKS" << endl;
+        cout << "===============" << endl;
+			
+	
+	
+
+}
 
