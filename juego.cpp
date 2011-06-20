41e3ab6f9ed35e8b95e85a17a5e5bb854dcbad2f
Fran Lucena <fran.lucena@gmail.com>
2011-06-20 20:17:45 +0200

    test for tablero class

--- juego.cpp
+++ juego.cpp
@@ -5,12 +5,31 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
+    
+pair<unsigned int,unsigned int> par(5,5);
+pair<unsigned int,unsigned int> pos(4,5);
+vector<char> aux(par.second,'X');
+list<char> lista1(25,'Z');
+
+tablero t(par);
+tablero t1(pos,lista1);
+
 	
 	cout << "BREAKING BLOCKS" << endl;
         cout << "===============" << endl;
-			
+
+        /*for (int i=0; i < par.second; i++)
+           cout << aux[i] << "_";
+        cout << t << endl;
+        
+        cout << "Posicion (4,4) = " << t.preguntar_posicion(pos) << endl;
+        */
+        
+        cout << t1 << endl;
+        
+        cout << endl;
 	
-	
+        cout << "===============" << endl;
 
 }
 
