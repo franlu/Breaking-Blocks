1535dc5d108766e6fa430ff6a2eaa7a21e07be62
Fran Lucena <fran.lucena@gmail.com>
2011-06-21 22:35:39 +0200

    head of members method

--- breaking_blocks.cpp
+++ breaking_blocks.cpp
@@ -1,1 +1,42 @@
+#include "breaking_blocks.h"
 
+breaking_blocks::breaking_blocks(const pair<unsigned int,unsigned int>& par){
+    
+    _tablero(par);
+    
+}
+
+breaking_blocks::breaking_blocks(const string& archivo){
+    
+    
+    
+}
+
+unsigned int breaking_blocks::eliminar_posicion(const pair<unsigned int,unsigned int>& par){
+    
+    
+    
+    
+}
+
+bool breaking_blocks::mas_movimientos() const {
+    
+    
+    
+    return true;
+}
+
+unsigned int breaking_blocks::bloques() const {
+    
+    unsigned int aux = 0;
+    
+    return aux;
+    
+}
+
+void breaking_blocks::imprimir_tablero() const{
+    
+    
+    cout << _tablero;
+    
+}
