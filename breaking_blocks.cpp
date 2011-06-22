f516e2e6633e6d6c73f848f4d58aeabc2050ac79
Fran Lucena <fran.lucena@gmail.com>
2011-06-22 13:48:20 +0200

    The Bloques method is OK

--- breaking_blocks.cpp
+++ breaking_blocks.cpp
@@ -28,6 +28,16 @@ bool breaking_blocks::mas_movimientos() const {
 unsigned int breaking_blocks::bloques() const {
     
     unsigned int aux = 0;
+    pair<unsigned int,unsigned int> tam = _tablero.size();
+    pair<unsigned int,unsigned int> par;
+    
+    for (int i=0; i< tam.first; i++)
+    	for (int j=0; j< tam.second; j++){
+            par.first = i;
+            par.second = j;
+            if (_tablero.preguntar_posicion(par) == '_')
+                aux++;
+	}
     
     return aux;
     
