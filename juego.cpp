ec9d6ed7787e1d56f1678e2e7c8b36ae711fa00e
Fran Lucena <fran.lucena@gmail.com>
2011-06-22 13:30:51 +0200

    test for tablero class

--- juego.cpp
+++ juego.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main(int argc, char *argv[]){
     
 pair<unsigned int,unsigned int> par(5,5);
-pair<unsigned int,unsigned int> pos(4,5);
+pair<unsigned int,unsigned int> pos(6,6);
 vector<char> aux(par.second,'X');
-list<char> lista1(25,'Z');
+list<char> lista1(36,'Z');
 
 tablero t(par);
 tablero t1(pos,lista1);
@@ -24,10 +24,8 @@ tablero t1(pos,lista1);
         
         cout << "Posicion (4,4) = " << t.preguntar_posicion(pos) << endl;
         */
-        
-        cout << t1 << endl;
-        
-        cout << endl;
+        cout << t1 <<endl;
+              
 	
         cout << "===============" << endl;
 
