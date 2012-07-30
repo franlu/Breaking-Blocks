213c08ad36409a863cb3f027c185c7c22867b096
Fran Lucena <fran.lucena@gmail.com>
2012-07-30 18:44:03 +0200

    [UP] test code

--- test/tab_vector.cpp
+++ test/tab_vector.cpp
@@ -1,86 +1,29 @@
 #include <iostream>
 
-#include "breaking_blocks.h"
+#include "../include/tablero.h"
 
 using namespace std;
 
-void menu(){
-    
-    cout << "1.- Eliminar Posicion." << endl;
-    cout << "2.- Es Posible eliminar mas bloques." << endl;
-    cout << "3.- Numero de bloques eliminados." << endl;
-    cout << "4.- Imprimir tablero." << endl;
-    cout << "5.- Puntuacion." << endl;
-    cout << "6.- Salir." << endl;
-     
-}
-
 int main(int argc, char *argv[]){
     
-pair<unsigned int,unsigned int> par_eli;    
-pair<unsigned int,unsigned int> par(5,5);
+    cout << endl << "   TEST TABLERO VECTOR   " << endl;
+            cout << "=========================" << endl;
+    
+    cout << endl << "tablero(const pair<unsigned int,unsigned int>& par);" << endl;
+            cout << "====================================================" << endl;
+                    pair<unsigned int,unsigned int> par(5,5);
+                    tablero t1(par);
+    cout << endl << "==========================OK========================" << endl;
+    
+    cout << endl << "tablero(const pair<unsigned int,unsigned int>& par, const list<char>& bloques);" << endl;
+            cout << "===============================================================================" << endl;
+                    pair<unsigned int,unsigned int> par2(2,2);
+                    list<char> bloques(4,'3');                    
+                    tablero t2(par2,bloques);
+    cout << endl << "================================================OK==============================" << endl;
 
-unsigned int opcion;
-int puntos = 0;
 
-   
-    string ruta;
-    cout << "Introduce la ruta hacia el fichero que contiene el tablero:" << endl;
-    cin >> ruta;
-    breaking_blocks bb(ruta);
-    cout << endl << "   BREAKING BLOCKS   " << endl;
-            cout << "=====================" << endl;
-                bb.imprimir_tablero();
-    cout << endl << "=====================" << endl;
-    
-    
-    menu();
-    cout << endl << "Elige opcion " << endl;
-    cin >> opcion;
-    
 
-while (opcion >0 && opcion < 6) {
-    
-        
-    switch(opcion){
-        
-        case 1:
-            cout << "Intoduce la coordenada i" << endl;
-            cin >> par_eli.first;
-            cout << "Intoduce la coordenada j" << endl;
-            cin >> par_eli.second;
-            
-            cout << "Has conseguido " << bb.eliminar_posicion(par_eli) << " puntos." << endl;
-            puntos += bb.eliminar_posicion(par_eli);
-            
-            break;
-        case 2:
-            if (bb.mas_movimientos())
-                cout << "Utiliza tus neuronillas, aún se pueden eliminar mas bloques" << endl;
-            else
-                cout << "Imposible eliminar mas bloques, consulta tu puntuacion." << endl;
-            break;
-        case 3:
-            cout << "Hasta el momento has eliminado " << bb.bloques() << "bloques." << endl;
-            break;
-        case 4:
-            cout << endl << "   BREAKING BLOCKS   " << endl;
-                    cout << "=====================" << endl;
-                            bb.imprimir_tablero();
-            cout << endl << "=====================" << endl;
-            break;
-        case 5:
-            cout << "Total:" << puntos << "puntos." << endl;
-            break;
-        default:
-            break;       
-        
-    }
-    
-    menu();
-    cout << endl << "Elige opcion " << endl;
-    cin >> opcion;
-}
 
 }
 
