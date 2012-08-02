ace6853db39d5057ae150ec129ac6348901928eb
Fran Lucena <fran.lucena@gmail.com>
2012-08-02 17:42:31 +0200

    [ADD] test preguntar_posicion

--- test/tab_vector.cpp
+++ test/tab_vector.cpp
@@ -18,8 +18,16 @@ int main(int argc, char *argv[]){
     cout << endl << "tablero(const pair<unsigned int,unsigned int>& par, const list<char>& bloques);" << endl;
             cout << "===============================================================================" << endl;
                     pair<unsigned int,unsigned int> par2(2,2);
-                    list<char> bloques(4,'3');                    
+                    list<char> bloques(4,' ');                    
                     tablero t2(par2,bloques);
+    cout << endl << "================================================OK==============================" << endl;
+    
+    cout << endl << "char preguntar_posicion(const pair<unsigned int,unsigned int>& par) const;" << endl;
+            cout << "===============================================================================" << endl;
+                    pair<unsigned int,unsigned int> par3(0,0);
+                    pair<unsigned int,unsigned int> par4(3,0);
+                    cout << t2.preguntar_posicion(par3) << endl;
+                    cout << t2.preguntar_posicion(par4) << endl;
     cout << endl << "================================================OK==============================" << endl;
 
 
