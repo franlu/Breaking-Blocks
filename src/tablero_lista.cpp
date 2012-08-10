364d1097e69a9d9ef6fe0f4d16802a9398745915
Fran Lucena <fran.lucena@gmail.com>
2012-08-10 12:49:36 +0200

    [UP] parameter constructor

--- src/tablero_lista.cpp
+++ src/tablero_lista.cpp
@@ -1,43 +1,53 @@
-#include "tablero.h"
+#include "../include/tablero.h"
 
 tablero::tablero(const pair<unsigned int,unsigned int>& par){
 
-	_tam = par;
-	list<char> aux(_tam.second);
-	for (int i = 0 ; i < _tam.first; i++ ){
-		for (int j = 0 ; j < _tam.second; j++ )
-			aux.push_back('X');
-		_matriz.push_back(aux);
-	}
+    _tam = par;
+    list<char> aux(_tam.second);
+    for (int i = 0 ; i < _tam.first; i++ ){
+        for (int j = 0 ; j < _tam.second; j++ )
+            aux.push_back('X');
+        _matriz.push_back(aux);
+}
 
 }
 
 tablero::tablero(const pair<unsigned int,unsigned int>& par, const list<char>& bloques){
 
-	_tam = par;
-	list<char>::iterator it;
-	for ( it=bloques.begin() ; it != bloques.end(); it++)
-																																	
+    _tam = par;
+    list<char> aux(_tam.second);
+    list<char>::const_iterator it;
+    unsigned int i;
+    for ( it=bloques.begin(),i=0; it != bloques.end(); it++,i++)
+        aux.push_back(*it);
+        if ((i % _tam.second) == 0){
+            _matriz.push_back(aux);
+            aux.clear(); 
+        }
+}
+
+tablero::tablero(const string& archivo){
+
 
 }
 
-tablero::preguntar_posicion(const pair<unsigned int,unsigned int>& par) const {
+char tablero::preguntar_posicion(const pair<unsigned int,unsigned int>& par) const {
 
-
-
+    char t = " ";
+    return t;
 
 }
 
 int tablero::eliminar_posicion(const pair<unsigned int,unsigned int>& par){
 
-
-
+    int i = 0;
+        return i;
 
 }
 
 pair<unsigned int,unsigned int> tablero::size() const {
 
-	return _tam;
+    return _tam;
 
 }
 
