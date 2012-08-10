513ea0d6d71267ab7eb8a69e3a276b05f2a61eec
Fran Lucena <fran.lucena@gmail.com>
2012-08-10 18:14:10 +0200

    [ADD] code for parameter constructor

--- src/tablero_lista.cpp
+++ src/tablero_lista.cpp
@@ -1,11 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "../include/tablero.h"
 
 tablero::tablero(const pair<unsigned int,unsigned int>& par){
 
     _tam = par;
     list<char> aux(_tam.second);
-    for (int i = 0 ; i < _tam.first; i++ ){
-        for (int j = 0 ; j < _tam.second; j++ )
+    for (unsigned int i = 0 ; i < _tam.first; i++ ){
+        for (unsigned int j = 0 ; j < _tam.second; j++ )
             aux.push_back('X');
         _matriz.push_back(aux);
 }
@@ -27,13 +32,37 @@ tablero::tablero(const pair<unsigned int,unsigned int>& par, const list<char>& b
 }
 
 tablero::tablero(const string& archivo){
-
+    
+    string palabra, str_local;
+    ifstream fich(archivo.c_str());
+    list<string> aux;
+    while (fich >> palabra) {
+        aux.push_back(palabra);
+    }
+    
+    fich.close();
+    
+    list<string>::const_iterator itr,itr2,itr3;
+    itr = aux.begin();
+    itr2 = ++itr;  
+    pair<unsigned int,unsigned int> par(atoi((*itr).c_str()),atoi((*itr2).c_str()));
+    _tam = par;
+    
+    string::iterator it;
+    list<char> lc;
+    for(itr3 = ++itr; itr3 != aux.end(); ++itr3){
+        str_local = *itr3;
+        for (it = str_local.begin(); it != str_local.end(); it++ )
+            lc.push_back(*it);
+        _matriz.push_back(lc);
+        lc.clear();
+    }
 
 }
 
 char tablero::preguntar_posicion(const pair<unsigned int,unsigned int>& par) const {
 
-    char t = " ";
+    char t;
     return t;
 
 }
@@ -57,14 +86,13 @@ ostream& operator<< (ostream& os, const tablero& t){
 pair<unsigned int,unsigned int> par;
 pair<unsigned int,unsigned int> tam = t.size();
 
-for (int i=0; i< tam.first; i++){
-		os << endl << "\t";
-		for (int j=0; j< tam.second; j++)
-			par.first = i;
-            par.second = j;
-            os << t.preguntar_posicion(par);
-	}
-*/
+for (unsigned int i=0; i< tam.first; i++){
+    os << endl << "\t";
+    for (unsigned int j=0; j< tam.second; j++){
+        par.first = i;
+        par.second = j;
+        os << t.preguntar_posicion(par);
+    }
+}    
 return os;
-
 }
